4cd17e680be92ed25dab0f29f3f2798d1455ff1c
konsolebox <konsolebox@gmail.com>
2018-07-14 08:50:30 +0800

    Add killtree-lite.bash.

---
+++ killtree-lite.bash
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# ------------------------------------------------------------
+#
+# killtree-lite
+#
+# A simpler version of killtree.
+#
+# Author: konsolebox
+# Copyright Free / Public Domain
+# July 14, 2018
+#
+# ------------------------------------------------------------
+
+shopt -uo posix && shopt -so noglob && shopt -s extglob || exit 1
+
+# killtree (pid, signal = 'SIGTERM')
+#
+function killtree {
+	if [[ $1 == -- ]]; then
+		shift; list+=("$@")
+
+		for __ do
+			killtree -- $(pgrep -P "$__")
+		done
+	else
+		local list=() IFS=$'\n' __
+		killtree -- "$1"
+		kill -s "${2-SIGTERM}" "${list[@]}"
+	fi
+}
+
+# main (['-s', signal,] pid, ...)
+#
+function main {
+	local sig=SIGTERM __
+
+	if [[ $1 == -s && -n $2 ]]; then
+		sig=$2
+		shift 2
+	fi
+
+	for __ do
+		if [[ $1 != +([[:digit:]]) ]]; then
+			echo "Usage: $0 [-s signal] pid ..." >&2
+			exit 1
+		fi
+	done
+
+	for __ do
+		killtree "$__" "${sig}"
+	done
+}
+
+main "$@"
