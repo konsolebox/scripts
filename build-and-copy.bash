c0ec2441b290347d582480bb01ae839f9a3ffe98
konsolebox <konsolebox@gmail.com>
2021-05-14 20:36:53 +0800

    Update build-and-copy.bash
    - Revise descriptions and messages
    - Add "create modules_list" feature

--- build-and-copy.bash
+++ build-and-copy.bash
@@ -4,25 +4,25 @@
 
 # build-and-copy.bash
 #
-# I use this script to automate building of initrd image
-# using the files and directories in the current directory.
+# This script automates building of initrd image using the
+# files and directories in the current directory.
 #
 # It also accepts a '-m' option which makes it copy kernel
 # modules to 'lib/modules' before creating the image.
 #
 # The initrd image is created in the parent directory and
-# copied to /boot.
+# then copied to /boot.
 #
 # Usage: [bash] ./build-and-copy.bash [-r|--kernel-release <release>] [-m|--copy-modules]"
 #
 # Author: konsolebox
 # Copyright Free / Public Domain
-# May 27, 2018
+# May 14, 2021
 
 # ----------------------------------------------------------
 
 if [ -z "${BASH_VERSION}" ]; then
-	echo 'You need bash to run this script.' >&2
+	echo "Bash is need to run this script." >&2
 	exit 1
 fi
 
@@ -48,22 +48,25 @@ function get_real_current_dir {
 	return 1
 }
 
-shopt -so pipefail || fail 'Failed to enable pipefail.'
+shopt -so pipefail || fail "Failed to enable pipefail."
 
 function main {
-	local copy_modules=false kernel_release= __
-	get_real_current_dir || fail 'Failed to get real current directory.'
+	local copy_modules=false create_modules_list=false kernel_release= __
+	get_real_current_dir || fail "Failed to get real current directory."
 	[[ $__ == / ]] && fail 'Refusing to run in /.'
 
 	while [[ $# -gt 0 ]]; do
 		case $1 in
 		-r|--kernel-release)
-			[[ -z $2 ]] && fail 'Kernel release not specified.'
+			[[ -z $2 ]] && fail "Kernel release not specified."
 			kernel_release=$2
 			shift
 			;;
 		-m|--copy-modules)
 			copy_modules=true
+			;;
+		-l|--create-modules-list)
+			create_modules_list=true
 			;;
 		-h|--help)
 			show_usage_and_exit
@@ -78,7 +81,7 @@ function main {
 
 	if [[ -z ${kernel_release} ]]; then
 		kernel_release=$(uname -r) && [[ -n ${kernel_release} ]] || \
-			fail 'Failed to get current kernel release.'
+			fail "Failed to get current kernel release."
 
 		echo "Using current kernel release which is '${kernel_release}'."
 	fi
@@ -98,6 +101,13 @@ function main {
 
 		cp -a "/lib/modules/${kernel_release}" lib/modules/ || \
 			fail "Failed to copy '/lib/modules/${kernel_release}' to 'lib/modules'."
+
+		if [[ ${create_modules_list} == true ]]; then
+			echo "Creating 'modules_list'."
+
+			find "/lib/modules/${kernel_release}" -name '*.ko' | \
+					gawk -F '[/.]' '{ print $(NF - 1) }' | sort -u > modules_list
+		fi
 	fi
 
 	local build=initramfs-${kernel_release}
