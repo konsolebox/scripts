abf76516f3429bee21580da67dc45f84e530da3a
konsolebox <konsolebox@gmail.com>
2021-05-14 20:04:53 +0800

    Update manfind.bash
    - Remove obvious comments
    - Make it work with older bash versions
    - Add `|| exit 1` next to `set -f`

--- manfind.bash
+++ manfind.bash
@@ -10,52 +10,36 @@
 #
 # Author: konsolebox
 # Copyright Free / Public Domain
-# May 27, 2018
+# May 14, 2021
 
 # ----------------------------------------------------------
-
-# Check if shell is Bash.
 
 [ -n "${BASH_VERSION}" ] || {
 	echo "This script requires Bash."
 	exit 1
 }
 
-# Disable filename expansion.
-
-set -f
-
-# Place everything inside a main function.
+set -f || exit 1
 
 function main {
-	# Check arguments.
-
-	[[ $# -eq 0 || $1 == '-h' || $1 == '--help' ]] && {
+	[[ $# -eq 0 || $1 == -h || $1 == --help ]] && {
 		echo "Usage: $0 keyword [keyword2 ...]"
 		return 1
 	}
 
-	# Prepare iname options.
-
-	local args=() __
+	local args paths IFS=: i=0 __
+	args=(); paths=()
 
 	for __; do
 		args=("${args[@]}" -iname "*$__*")
 	done
 
-	# Prepare paths.
-
-	local paths=() IFS=: i=0
-
-	for __ in $MANPATH; do
+	for __ in ${MANPATH}; do
 		[[ -n $__ && ":${paths[*]}:" != *":$__:"* ]] && paths[i++]=$__
 	done
 
-	# Find.
-
-	[[ ${#paths[@]} -gt 0 ]] && find "${paths[@]}" -maxdepth 2 -xtype f -regex '.*/man[^/]+/.*' "${args[@]}" 2>/dev/null
+	[[ ${#paths[@]} -gt 0 ]] && find "${paths[@]}" -maxdepth 2 -xtype f -regex '.*/man[^/]+/.*' \
+		"${args[@]}" 2>/dev/null
 }
-
-# Start.
 
 main "$@"
