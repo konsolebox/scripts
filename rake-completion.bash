41de52e5831a0b026a8b018bb5751d87fea0299f
konsolebox <konsolebox@gmail.com>
2022-02-28 03:47:38 +0800

    rake-completion.bash: Multiple updates
    - Allow completion of multiple tasks
    - Always quote results unless word was separated with " or '
    - Always return 1 if _rake_comp_get_opts_with_arg_expr fails
    - Limit comment width to 72
    - Make opt with arg extraction more accurate
    - Use 'compopt -o nospace'

--- rake-completion.bash
+++ rake-completion.bash
@@ -102,44 +102,77 @@ if [[ BASH_VERSINFO -ge 5 ]]; then
 		return 0
 	}
 
-	function _rake_comp_task_likely_specified {
-		local opts_with_arg_expr= i __
-		_rake_comp_get_opts_with_arg_expr && opts_with_arg_expr=$__
+	function _rake_comp_generate_filename_replies {
+		local one_final_result=false temp i gen_type=${2-f}
+		readarray -t COMPREPLY < <(compgen -"${gen_type}" -- "$1")
 
-		for (( i = 1; i < ${#COMP_WORDS[@]}; ++i )); do
-			case ${COMP_WORDS[i]} in
-			--)
-				break
-				;;
-			+([a-z0-9_])?(:)*)
-				[[ i -ne COMP_CWORD ]] && return 0
-				;;
-			${opts_with_arg_expr})
+		while [[ ${#COMPREPLY[@]} -eq 1 && -d ${COMPREPLY} && -x ${COMPREPLY} ]]; do
+			readarray -t temp < <(cd "${COMPREPLY}" &>/dev/null && compgen -"${gen_type}")
+			[[ ${#temp[@]} -eq 0 ]] && break
+			COMPREPLY=("${temp[@]/#/"${COMPREPLY%/}/"}")
+		done
+
+		[[ ${#COMPREPLY[@]} -eq 1 ]] && test -"${gen_type}" "${COMPREPLY}" && one_final_result=true
+
+		for i in "${!COMPREPLY[@]}"; do
+			[[ -d ${COMPREPLY[i]} ]] && COMPREPLY[i]=${COMPREPLY[i]%%+(/)}/
+		done
+
+		[[ ${one_final_result} == true ]]
+	}
+
+	function _rake_comp_past_double_dash {
+		local i
+
+		for (( i = 1; i < COMP_CWORD; ++i )); do
+			[[ ${COMP_WORDS[i]} == -- ]] && return 0
+		done
+
+		return 1
+	}
+
+	function _rake_comp_try_get_opt_with_arg {
+		local -n __opt=$1 __arg=$2 __prefix=$3
+		local i __
+		_rake_comp_get_opts_with_arg_expr || return 1
+
+		for (( i = 1; i <= COMP_CWORD; ++i )); do
+			set -- "${COMP_WORDS[@]:i:2}"
+
+			if [[ i -eq COMP_CWORD && $1 == --* && $1 == @($__)=* ]]; then
+				__arg=${1#*=} __prefix=${1:0:(${#1} - ${#__arg})} __opt=${__prefix%=}
+				return 0
+			elif [[ i -eq COMP_CWORD && ${#1} -gt 2 && $1 == -[!-]* && $1 == @($__)* ]]; then
+				__arg=${1#*=} __prefix=${1:0:(${#1} - ${#__arg})} __opt=$__prefix
+				return 0
+			elif [[ $1 == @($__) ]]; then
+				if (( i == COMP_CWORD - 1 )); then
+					__opt=$1 __arg=$2 __prefix=
+					return 0
+				fi
+
 				(( ++i ))
-				;;
-			esac
+			fi
 		done
 
 		return 1
 	}
 
 	function _rake_comp_get_specified_rakefile {
-		local opts_with_arg_expr=
-		_rake_comp_get_opts_with_arg_expr && opts_with_arg_expr=$__
-		set -- "${COMP_WORDS[@]}"
+		_rake_comp_get_opts_with_arg_expr || return 1
+		local opts_with_arg_expr=$__
 		__=
 
-		while shift; [[ $# -gt 0 && $1 != -- ]]; do
-			case $1 in
+		for (( i = 1; i < ${#COMP_WORDS[@]}; ++i )); do
+			case ${COMP_WORDS[i]} in
 			-f|--rakefile)
-				__=$2
-				shift
+				__=${COMP_WORDS[++i]-}
 				;;
 			-f*|--rakefile=*)
-				__=${1#@(-f|--rakefile=)}
+				__=${COMP_WORDS[i]#@(-f|--rakefile=)}
 				;;
 			${opts_with_arg_expr})
-				shift
+				(( ++i ))
 				;;
 			esac
 		done
@@ -171,82 +204,83 @@ if [[ BASH_VERSINFO -ge 5 ]]; then
 		return 1
 	}
 
-	function _rake_comp_generate_filename_replies {
-		local one_final_result=false temp i gen_type=${2-f}
-		readarray -t COMPREPLY < <(compgen -"${gen_type}" -- "$1")
-
-		while [[ ${#COMPREPLY[@]} -eq 1 && -d ${COMPREPLY} && -x ${COMPREPLY} ]]; do
-			readarray -t temp < <(cd "${COMPREPLY}" &>/dev/null && compgen -"${gen_type}")
-			[[ ${#temp[@]} -eq 0 ]] && break
-			COMPREPLY=("${temp[@]/#/"${COMPREPLY%/}/"}")
-		done
-
-		[[ ${#COMPREPLY[@]} -eq 1 ]] && test -"${gen_type}" "${COMPREPLY}" && one_final_result=true
-
-		for i in "${!COMPREPLY[@]}"; do
-			[[ -d ${COMPREPLY[i]} ]] && COMPREPLY[i]=${COMPREPLY[i]%%+(/)}/
-			printf -v "COMPREPLY[$i]" %q "${COMPREPLY[i]}"
-		done
-
-		[[ ${one_final_result} == true ]]
-	}
-
-	function rake_comp_get_tasks {
-		local ts
+	function _rake_comp_get_available_tasks {
+		local rakefile=$1 ts IFS=$'\n'
+		local -n __r=$2
 
 		if [[ ${_RAKE_COMP_CACHE_TASKS} == true ]]; then
-			ts=$(date +%s -r "$1") || ts=
-			__=${_RAKE_COMP_TASK_CACHE[$1]-}
-			[[ $__ && ${ts} && ${_RAKE_COMP_TASK_CACHE_TS[$1]-} == ${ts} ]] && return 0
+			ts=$(date +%s -r "${rakefile}") || ts=
+			readarray -t __r <<< "${_RAKE_COMP_TASK_CACHE[$1]-}"
+			[[ ${#__r[@]} && ${ts} && ${_RAKE_COMP_TASK_CACHE_TS[$1]-} == ${ts} ]] && return 0
 		fi
 
-		__=$("${_RAKE_PATH}" -f "$1" --tasks 2>&1 | awk '$1 == "rake" && / # / { print $2 }')
+		readarray -t __r < <("${_RAKE_PATH}" -f "${rakefile}" --tasks 2>&1 | \
+				awk '$1 == "rake" && / # / { print $2 }')
 
-		if [[ $__ && ${_RAKE_COMP_CACHE_TASKS} == true && ${ts} ]]; then
-			_RAKE_COMP_TASK_CACHE[$1]=$__
-			_RAKE_COMP_TASK_CACHE_TS[$1]=${ts}
+		if [[ ${#__r[@]} -gt 0 && ${_RAKE_COMP_CACHE_TASKS} == true && ${ts} ]]; then
+			_RAKE_COMP_TASK_CACHE[${rakefile}]=${__r[*]}
+			_RAKE_COMP_TASK_CACHE_TS[${rakefile}]=${ts}
 		fi
 
-		[[ $__ ]]
+		[[ ${#__r[@]} -gt 0 ]]
 	}
 
-	function _rake_comp_past_double_dash {
+	function _rake_comp_get_specified_tasks {
+		local -n __r=$1; __r=()
+		local IFS=' ' i __
+		_rake_comp_get_opts_with_arg_expr || return 1
+
+		for (( i = 1; i < ${#COMP_WORDS[@]}; ++i )); do
+			if [[ ${COMP_WORDS[i]} == -- ]]; then
+				break
+			elif [[ ${COMP_WORDS[i]} == @($__) ]]; then
+				(( ++i ))
+			elif [[ ${COMP_WORDS[i]} != -* && i -ne COMP_CWORD ]]; then
+				__r+=("${COMP_WORDS[i]}")
+			fi
+		done
+
+		[[ "${__r[*]}" == *[\'\"\\]* ]] && readarray -t __r < <(compgen -W "${__r[*]}")
+		[[ ${#__r[@]} -gt 0 ]]
+	}
+
+	function _rake_comp_get_unspecified_task_replies {
+		local prefix=$1 rakefile=$2 a_tasks s_tasks IFS=$'\n'
+		local -A u_tasks=()
+		_rake_comp_get_available_tasks "${rakefile}" a_tasks || return 1
+		_rake_comp_get_specified_tasks s_tasks || s_tasks=()
+
+		for __ in "${a_tasks[@]}"; do
+			u_tasks[$__]=$__
+		done
+
+		for __ in "${s_tasks[@]}"; do
+			unset 'u_tasks[$__]'
+		done
+
+		readarray -t COMPREPLY < <(compgen -W "${u_tasks[*]}" -- "${prefix}")
+		[[ ${#COMPREPLY[@]} -gt 0 ]]
+	}
+
+	function _rake_comp_current_word_open_quoted {
 		local i
 
-		for (( i = 1; i < COMP_CWORD; ++i )); do
-			[[ ${COMP_WORDS[i]} == -- ]] && return 0
+		for (( i = COMP_POINT - 1; i > 0; --i )); do
+			[[ ${COMP_LINE:i:1} == [\"\'] ]] && return 0
+			[[ ${COMP_LINE:i:1} == [${COMP_WORDBREAKS}] ]] && break
 		done
 
 		return 1
 	}
 
-	function _rake_comp_try_get_opt_with_arg {
-		local -n __opt=$1 __arg=$2 __prefix=$3; shift 3
-		_rake_comp_get_opts_with_arg_expr
-
-		if [[ ${2-} == @($__) ]]; then
-			__opt=$2 __arg=$1 __prefix=
-		elif [[ $1 == @($__) ]]; then
-			__opt=$1 __arg= __prefix=$1
-		elif [[ $1 == --* && $1 == @($__)=* ]]; then
-			__arg=${1#*=} __prefix=${1:0:(${#1} - ${#__arg})} __opt=${__prefix%=}
-		elif [[ $1 != --* && $1 == @($__)* ]]; then
-			__arg=${1#*=} __prefix=${1:0:(${#1} - ${#__arg})} __opt=$__prefix
-		else
-			return 1
-		fi
-
-		return 0
-	}
-
 	function _rake_comp {
-		local dont_add_space=false opt arg prefix i __
+		local dont_add_space=false i opt arg prefix u_tasks __
 		_RAKE_PATH=$(type -p rake) && [[ ${_RAKE_PATH} ]] || return
 		COMPREPLY=()
 
 		if _rake_comp_past_double_dash; then
 			_rake_comp_generate_filename_replies "$2" || dont_add_space=true
-		elif _rake_comp_try_get_opt_with_arg opt arg prefix "${@:2}"; then
+		elif _rake_comp_try_get_opt_with_arg opt arg prefix; then
 			case ${opt} in
 			-C|--directory|-I|--libdir|-R|--rakelibdir)
 				_rake_comp_generate_filename_replies "${arg}" d || dont_add_space=true
@@ -264,27 +298,32 @@ if [[ BASH_VERSINFO -ge 5 ]]; then
 		elif [[ $2 == -* ]]; then
 			_rake_comp_get_all_opts || return
 			readarray -t COMPREPLY < <(compgen -W "$__" -- "$2")
-		elif ! _rake_comp_task_likely_specified && { _rake_comp_get_specified_rakefile || \
-				_rake_comp_get_default_rakefile; }; then
-			rake_comp_get_tasks "$__" || return
-			readarray -t COMPREPLY < <(compgen -W "$__" -- "$2")
+		elif _rake_comp_get_specified_rakefile || _rake_comp_get_default_rakefile; then
+			_rake_comp_get_unspecified_task_replies "$2" "$__" || return
 		fi
 
-		[[ ${#COMPREPLY[@]} -eq 1 && ${dont_add_space} == false ]] && COMPREPLY+=' '
+		if ! _rake_comp_current_word_open_quoted; then
+			for i in "${!COMPREPLY[@]}"; do
+				printf -v "COMPREPLY[$i]" %q "${COMPREPLY[i]}"
+			done
+		fi
+
+		[[ ${dont_add_space} == true ]] && compopt -o nospace
 	}
 
-	# Excluding ':' from COMP_WORDBREAKS is necessary so tasks that are named
-	# with a colon can be recognized, but it conflicts with other completion
-	# implementations that require it like git's contrib/git-completion.bash.
+	# Excluding ':' from COMP_WORDBREAKS is necessary so tasks that are
+	# named with a colon can be recognized, but it conflicts with other
+	# completion implementations that require it like git's
+	# contrib/git-completion.bash.
 	#
 	# Comment out the following line if the conflict needs to be avoided.
 	#
 	COMP_WORDBREAKS=${COMP_WORDBREAKS//:}
 
-	# Removing '=' is also necessary since the equal sign still becomes stored
-	# as a separate argument in COMP_WORDS.  Besides that, the equal sign can
-	# also be a part of the filename and even though COMP_WORDS can be
-	# recomposed using `compgen -W`, telling bash how the token should be
+	# Removing '=' is also necessary since the equal sign still becomes
+	# stored as a separate argument in COMP_WORDS.  Besides that, the equal
+	# sign can also be a part of the filename and even though COMP_WORDS can
+	# be recomposed using `compgen -W`, telling bash how the token should be
 	# completed itself would require an ugly workaround since the token is
 	# already split.  Perhaps the replies can be trimmed out so they don't
 	# include the partial strings which aren't originally part of the token
@@ -293,5 +332,5 @@ if [[ BASH_VERSINFO -ge 5 ]]; then
 	#
 	COMP_WORDBREAKS=${COMP_WORDBREAKS//=}
 
-	complete -F _rake_comp -o nospace rake
+	complete -F _rake_comp rake
 fi
