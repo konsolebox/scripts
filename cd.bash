e6e35e45b2151dbd26c4929e3ed3fa99792e8a00
konsolebox <konsolebox@gmail.com>
2024-06-17 03:27:56 +0800

    cd.bash: Multiple updates
    - Ask user which directory argument to use
    - Don't expand single-character parameter "$@" with braces
    - Simplify description

--- cd.bash
+++ cd.bash
@@ -1,22 +1,23 @@
 # ----------------------------------------------------------------------
+#
 # cd.bash
 #
-# Provides a convenient enhancement wrapper for the builtin cd command
-# so it dynamically uses the directory stack instead of just OLDPWD.
-# The wrapper also makes directories physically resolved by default.
-# If '.' is specified, the value of "$PWD" is passed instead.  This is
-# useful when currently directory's mount status has been updated.
-# Always converting relative paths to complete paths has been considered
-# but not doing it is thought to be the safer choice.
+# Provides an enhancement wrapper for the builtin cd command.
+#
+# - It refers to the directory stack first before the OLDPWD variable.
+# - Resolves directories to their physical versions by default.
+# - If '.' is specified, the value of "$PWD" is passed instead.
+# - If multiple directory arguments are passed and shell is interactive,
+#   the user is asked to select one.
 #
 # Author: konsolebox
 # Copyright Free / Public Domain
-# May 13, 2022
+# June 17, 2024
 #
 # ----------------------------------------------------------------------
 
 function cd {
-	local args=() opts=()
+	local args=() opts=() __
 
 	while [[ $# -gt 0 ]]; do
 		case $1 in
@@ -38,6 +39,17 @@ function cd {
 	set -- "${args[@]}"
 
 	{
+		if [[ $# -gt 1 && $- == *i* && -t 0 && -t 2 ]]; then
+			echo "Choose a directory." >&2
+
+			select __; do
+				if [[ $__ ]]; then
+					set -- "$__"
+					break
+				fi
+			done
+		fi
+
 		if [[ $# -gt 1 ]]; then
 			echo "Too many directory argmuments specified." >&2
 		elif [[ ${1-} == - && ${#DIRSTACK[@]} -ge 2 ]]; then
@@ -51,7 +63,7 @@ function cd {
 			pushd -n -- "${PWD}"
 			[[ ${1-} == . ]] && set -- "${PWD}"
 
-			if builtin cd -P "${opts[@]}" -- "${@}"; then
+			if builtin cd -P "${opts[@]}" -- "$@"; then
 				[[ ${DIRSTACK[0]} == "${DIRSTACK[1]}" ]] && popd -n
 				return 0
 			fi
